73105002812b32d9b164126f6161eec1c01b8462
Carlos Grahl <cgrahl@gmail.com>
2017-09-04 06:18:35 -0300

    adicionando security

--- clientes/src/main/java/br/com/grahl/clientes/repository/ClienteRepository.java
+++ clientes/src/main/java/br/com/grahl/clientes/repository/ClienteRepository.java
@@ -2,7 +2,9 @@ package br.com.grahl.clientes.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 import br.com.grahl.clientes.domain.Cliente;
@@ -11,5 +13,9 @@ import br.com.grahl.clientes.domain.Cliente;
 public interface ClienteRepository extends CrudRepository<Cliente, String>{
 	
 	List<Cliente> findAllByOrderByNome();
+	
+	@Query("from Cliente c where lower(c.nome) like CONCAT('%', lower(:nome), '%')")
+	List<Cliente> findByNome(@Param("nome") String nome);
+
 
 }
---
+++ clientes/src/main/java/br/com/grahl/clientes/security/model/security/AuthorityName.java
@@ -0,0 +1,5 @@
+package br.com.grahl.clientes.security.model.security;
+
+public enum AuthorityName {
+    ROLE_USER, ROLE_ADMIN
+}
